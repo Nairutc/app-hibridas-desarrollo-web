7a5e9067009fd9511d6dd104051d17ba4f0eb603
Nairutc <nairutc@gmail.com>
2026-08-12 15:43:51 -0400

    practica actividad-01

---
+++ semana-01/ProductManager.js
@@ -0,0 +1,38 @@
+class ProductManager {
+    constructor() {
+        this.products = [];
+    }
+
+    addProduct(product) {
+        if (!product.id || !product.name || !product.description || !product.price || !product.stock) {
+        console.log("Error: faltan campos obligatorios");
+        return;
+        }
+
+        const exists = this.products.find((p) => p.id === product.id);
+
+        if (exists) {
+        console.log("Error: el id ya existe");
+        return;
+        }
+
+        this.products.push(product);
+    }
+
+    getProducts() {
+        return this.products;
+    }
+
+    getProductById(id) {
+        const product = this.products.find((p) => p.id === id);
+
+        if (!product) {
+        console.log("Not found");
+        return;
+        }
+
+        return product;
+    }
+    }
+
+    module.exports = ProductManager;
---
+++ semana-01/app.js
@@ -0,0 +1,25 @@
+const ProductManager = require("./ProductManager");
+
+const manager = new ProductManager();
+
+    manager.addProduct({
+    id: 1,
+    name: "Notebook",
+    description: "Notebook Lenovo",
+    price: 1200,
+    stock: 10,
+    });
+
+    manager.addProduct({
+    id: 2,
+    name: "Mouse",
+    description: "Mouse inalambrico",
+    price: 25,
+    stock: 50,
+    });
+
+    console.log(manager.getProducts());
+
+    console.log(manager.getProductById(1));
+
+    console.log(manager.getProductById(99));
