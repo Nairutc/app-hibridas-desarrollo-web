846ae1efc0b055d06724f228885b682311b375e6
Nairut Contreras <nairutc@gmail.com>
2026-08-29 12:24:29 -0400

    semana_02 servidor con Express(actualizado)

--- semana_02/Users.js
+++ semana_02/Users.js
@@ -22,16 +22,43 @@ class Users {
     this.saveUsers();
     return user;
     }
-
     getUsers() {
     return this.users;
     }
-
+    getUserById(id) {
+    const user = this.users.find((user) => user.id === id); 
+    return user;
+    }
+    upadateUser(id,user){
+    const index = this.users.findIndex((user) => user.id === id); 
+    if( index === -1 ) {
+        console.error("Not found");
+        return user;
+    }
+    if (user.name){
+        this.users[index].name = user.name;
+    }
+    if (user.email){
+        this.users[index].email = user.email;
+    }
+    if (user.password){
+        this.users[index].password = user.password;
+    }
+        this.saveUsers()
+    }
+    deleteUserById(id) {
+    const index = this.users.findIndex((user) => user.id === id); 
+    if( index === -1 ) {
+        console.error("No se encontró el usuario con id: " + id);
+        return;
+    }
+    this.users.splice(index,1);
+    return 'ok';
+    }
     saveUsers(){
         const data = JSON.stringify( this.users, null, 2);
         fs.writeFileSync(this.path, data , 'utf-8');
     }
-
     loadUsers(){
         //validamos que el archivo existe
         if ( !fs.existsSync(this.path) ){
